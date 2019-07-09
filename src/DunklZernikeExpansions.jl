8ae03894e408deaf676c1a1cdb2e8dc14365b559
Gonzalo <gobenavides@udec.cl>
2019-07-09 17:23:05 -0400

    Add copyright notice and permission notice of FastTransforms.jl

--- src/DunklZernikeExpansions.jl
+++ src/DunklZernikeExpansions.jl
@@ -182,7 +182,18 @@ end
 
 """
 Pochhammer symbol ``(x)_n = \\frac{\\Gamma(x+n)}{\\Gamma(x)}`` for the rising factorial.
-Taken from https://github.com/JuliaApproximation/FastTransforms.jl/blob/master/src/specialfunctions.jl#L33
+Taken from FastTransforms.jl under the MIT "Expat" License:
+
+    Copyright (c) 2016-2019: Richard Mikael Slevinsky and other contributors:
+
+    https://github.com/JuliaApproximation/FastTransforms.jl/graphs/contributors
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 """
 function pochhammer(x::Number,n::Integer)
     ret = one(x)
