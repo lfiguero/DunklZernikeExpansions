0fbac9e1dcb7f3c28f93d09d2484c11f0d0ce3a5
Gonzalo Benavides <gobenavides@udec.cl>
2020-06-17 15:30:50 -0400

    Adress conflicting case in the computation of the ratio of the weighted square norm between two consecutive Jacobi polynomials of same parameters.

--- src/DunklZernikeExpansions.jl
+++ src/DunklZernikeExpansions.jl
@@ -309,8 +309,13 @@ end
 """
 Compute the ratio between the weighted square norms of two consecutive Jacobi polynomials of same parameters
 """
-JacDegreeRatio(n::Integer,α::Float64,β::Float64) = ((2n+α+β+1)/(2n+α+β+3))*((n+α+1)/(n+α+β+1))*((n+β+1)/(n+1))
-
+function JacDegreeRatio(n::Integer,α::Float64,β::Float64)
+	if n == 0 && α+β+1≈0
+		((α+1)/(α+β+3))*(β+1)
+	else
+		((2n+α+β+1)/(2n+α+β+3))*((n+α+1)/(n+α+β+1))*((n+β+1)/(n+1))
+	end
+end
 """
 Compute the ratio between twe weighted square norm of two Jacobi polynoamials of same degree and first parameter but differing in its second parameter in two units
 """
